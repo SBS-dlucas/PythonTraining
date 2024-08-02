e2fec10c85568937b18f972da80eff5f96b6954c
Dane Lucas <dlucas@seabird.com>
2024-08-02 10:19:22 -0700

    Some updates for macos

--- src/sockettesting.py
+++ src/sockettesting.py
@@ -27,6 +27,7 @@ def send_command(sid, data):
 def disconnect(sid):
     print('disconnect ', sid)
 
+
 def start_socket_server(port):
     try:
         eventlet.wsgi.server(eventlet.listen(('127.0.0.1', port)), app)
@@ -38,11 +39,9 @@ def start_socket_server(port):
 if __name__ == '__main__':
     print('starting server 1 on 65432')
     eventlet.spawn(start_socket_server, 65432)
-   # t1 = threading.Thread(target=start_socket_server, args=[65432])
-   # t1.start()
 
     print('starting server 2 on 65433')
-    start_socket_server(65433)
-   # eventlet.spawn(start_socket_server, [65433])
-   # t2 = threading.Thread(target=start_socket_server, args=[65433])
-   # t2.start()
+    eventlet.spawn(start_socket_server(65433))
+
+    # If we need to spawn more servers, continue with spawning them on threads
+    # eventlet.spawn(start_socket_server, [65433])
